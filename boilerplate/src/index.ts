6aa2e312aeca3b4be570ad31f9ccc2eb7845d872
ARASH AHMADI <arash.a@live.se>
2021-05-18 17:31:32 +0200

    #6

--- boilerplate/src/index.ts
+++ boilerplate/src/index.ts
@@ -1,1 +1,31 @@
 console.log("hello world!");
+
+// import { Timer } from 'easytimer.js';
+// import * as timerDigital from './timerDigital';
+
+// let timer: Timer = new Timer();
+
+// //on click start timer
+// timer.start({countdown: true, startValues: {seconds: 10}})
+
+// let appEl: any = document.querySelector('#app');
+
+// timer.on('secondsUpdated', () => {
+//     console.log(timer.getTimeValues().seconds);
+//     console.log(timer.getTimeValues().minutes);
+//     appEl.innnerhtml = '';
+//     appEl.insertAdjacentHTML('beforeend', timerDigital.render(timer));
+// })
+
+// timer.on('targetAchieved', () => {
+//     console.log('wheeyy')
+// })
+
+
+// function render(timer: any, smg?: string) :string {
+//     let template:string = `
+//     <section id="timer-digital>
+//      <h1>${timer.getTimeValues().minuties}</h1>
+//     </section>
+//     `
+// }
